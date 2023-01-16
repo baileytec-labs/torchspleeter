e44d34b11b2f8cefa634bf1d61cd9ff5dcafc7a3
sean-bailey <seanbailey518@gmail.com>
2023-01-16 11:57:51 -0500

    added more flexibility and demonstration to the tests

--- test_estimator.py
+++ test_estimator.py
@@ -1,8 +1,9 @@
 import numpy as np
 import librosa
 import soundfile
-import torch 
-
+import torch
+import pydub
+import os
 from spleeter.estimator import Estimator
 
 if __name__ == '__main__':
@@ -13,8 +14,22 @@ if __name__ == '__main__':
     es.eval()
 
     # load wav audio
-    wav, _ = librosa.load('./audio_example.mp3', mono=False, res_type='kaiser_fast',sr=sr)
+    testaudiofile="./audio_example.mp3"
+    filestats=pydub.AudioSegment.from_file(testaudiofile)
+    channels=filestats.channels
+    mono_selection=False
+    if channels==1:
+        mono_selection=True
+        multichannel=pydub.AudioSegment.from_mono_audiosegments(filestats,filestats)
+        testaudiofile=testaudiofile.split('.')[0]+"-stereo."+testaudiofile.split('.')[-1]
+        multichannel.export(out_f=testaudiofile,format=testaudiofile.split('.')[-1])
+    print(mono_selection)
+    print(channels)
+    wav, _ = librosa.load(testaudiofile, mono=False, res_type='kaiser_fast',sr=sr)
     wav = torch.Tensor(wav).to(device)
+    if mono_selection:
+        os.remove(testaudiofile)
+
 
     # normalize audio
     # wav_torch = wav / (wav.max() + 1e-8)
