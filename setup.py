1f31ff449993d1376960194a15f78f96749d6f8e
sean-bailey <seanbailey518@gmail.com>
2023-01-17 08:10:36 -0500

    triggering build properly

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ print(find_packages())
 
 setup(
     name='torchspleeter',
-    version='0.1.2',
+    version='0.1.3',
     packages=find_packages(),
     include_package_data=True,
     entry_points={
